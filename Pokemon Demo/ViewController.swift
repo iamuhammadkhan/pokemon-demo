7ae330b8d718913fe57b70ce64e2d8013d6601ac
Muhammad Khan <imuhammad.khan@icloud.com>
2022-02-10 13:50:51 +0500

    Initial Commit

---
+++ Pokemon Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon Demo
+//
+//  Created by Muhammad Khan on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
